ebad32ea9160ce5b9083290558cdad5d7353a022
Ilya Kucher <dozer2001@mail.ru>
2018-10-25 21:41:44 +0300

    DOP

--- Dop/js/script.js
+++ Dop/js/script.js
@@ -1,22 +1,32 @@
-
 let inputRub = document.getElementById('rub'),
     inputUsd = document.getElementById('usd');
 
 inputRub.addEventListener('input', () => {
     let request = new XMLHttpRequest();
-
     request.open('GET', 'js/current.json');
     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     request.send();
-    
-    request.addEventListener('readystatechange', function() {
-        if (request.readyState === 4 && request.status == 200) {
-            let data = JSON.parse(request.response);
+    request.addEventListener('readystatechange', function () {
+        function o() {
+            return new Promise(function (resolve, reject) {
+                if (request.readyState === 4 && request.status == 200) {
+                    resolve();
+                    // inputUsd.value = inputRub.value / data.usd;
+                } else {
+                    reject();
+                    // inputUsd.value = "Что-то пошло не так!";
+                }
 
-            inputUsd.value = inputRub.value / data.usd;
-        } else {
-            inputUsd.value = "Что-то пошло не так!";
+            });
         }
-    });
+        o()
+            .then(() => {
+                let data = JSON.parse(request.response);
+                inputUsd.value = inputRub.value / data.usd;
+            })
+            .catch(() => {
 
+                inputUsd.value = "Что-то пошло не так!";
+            })
+    });
 });
